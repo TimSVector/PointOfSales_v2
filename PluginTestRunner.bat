65579c4e70707ec018b506d9bc16908c68e4b9e2
TimSVector <tim.schneider@vector.com>
2020-03-09 16:11:19 -0400

    More updates for parameters

--- PluginTestRunner.bat
+++ PluginTestRunner.bat
@@ -3,9 +3,22 @@ xcopy /S /Q /Y C:\Users\vaprti\vector\github\vectorcast-execution-plugin-tms-pip
 set path=%PATH%;C:\vector\tools\gnat\2019\bin
 set WORKSPACE=%CD%
 
-%VECTORCAST_DIR%/manage --project %1 --build-execute --incremental --output RebuildReport.html > build.log
-%VECTORCAST_DIR%/manage --project %1 --create-report=metrics --output=MetricsReport.html
-%VECTORCAST_DIR%/manage --project %1 --full-status=FullStatusReport.html
+set PROJECT=%1
+set COMPILER=%2
+set TESTSUITE=%3
+set ENVIRONMENT=%4
 
-%VECTORCAST_DIR%/vpython vc_scripts/generate-results.py %1 --junit --buildlog build.log
-%VECTORCAST_DIR%/vpython vc_scripts/generate-results.py %1 --final
+if "%COMPILER%" == "" and  and "%ENVIRONMENT%" == "" (
+    if "%TESTSUITE%" == "" (
+        %VECTORCAST_DIR%/manage --project %PROJECT% --build-execute --incremental --output %PROJECT%_RebuildReport.html > build.log
+    ) else (
+        %VECTORCAST_DIR%/manage --project %PROJECT% --build-execute --incremental --level %COMPILER%/%TESTSUITE% --output %PROJECT%_%COMPILER%_%TESTSUITE%_RebuildReport.html > build.log
+    )
+) else (
+   %VECTORCAST_DIR%/manage --project %PROJECT% --build-execute --incremental --level %COMPILER%/%TESTSUITE% --environment %ENVIRONMENT% --output %PROJECT%_%COMPILER%_%TESTSUITE%_%ENVIRONMENT%_RebuildReport.html > build.log
+)
+%VECTORCAST_DIR%/manage --project %PROJECT% --create-report=metrics --output=MetricsReport.html
+%VECTORCAST_DIR%/manage --project %PROJECT% --full-status=FullStatusReport.html
+
+%VECTORCAST_DIR%/vpython vc_scripts/generate-results.py %PROJECT% --junit --buildlog build.log
+%VECTORCAST_DIR%/vpython vc_scripts/generate-results.py %PROJECT% --final
