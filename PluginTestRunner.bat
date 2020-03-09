67a435b5a24a7a40a7d699f78b25873a13fe34be
TimSVector <tim.schneider@vector.com>
2020-03-09 15:34:18 -0400

    updates

--- PluginTestRunner.bat
+++ PluginTestRunner.bat
@@ -3,9 +3,9 @@ xcopy /S /Q /Y C:\Users\vaprti\vector\github\vectorcast-execution-plugin-tms-pip
 set path=%env.Path%;C:\vector\tools\gnat\2019\bin
 set WORKSPACE=%system.teamcity.build.workingDir%
 
-%env.VECTORCAST_DIR%/manage --project %1 --build-execute --incremental --output RebuildReport.html > build.log
-%env.VECTORCAST_DIR%/manage --project %1 --create-report=metrics --output=MetricsReport.html
-%env.VECTORCAST_DIR%/manage --project %1 --full-status FullStatusReport.html
+%VECTORCAST_DIR%/manage --project %1 --build-execute --incremental --output RebuildReport.html > build.log
+%VECTORCAST_DIR%/manage --project %1 --create-report=metrics --output=MetricsReport.html
+%VECTORCAST_DIR%/manage --project %1 --full-status FullStatusReport.html
 
-%env.VECTORCAST_DIR%/vpython vc_scripts/generate-results.py %1 --junit --buildlog build.log
-%env.VECTORCAST_DIR%/vpython vc_scripts/generate-results.py %1 --final
+%VECTORCAST_DIR%/vpython vc_scripts/generate-results.py %1 --junit --buildlog build.log
+%VECTORCAST_DIR%/vpython vc_scripts/generate-results.py %1 --final
