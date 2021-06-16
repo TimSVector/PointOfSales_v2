9bde095d923751b2b5253c28ef3a71e005aead7c
TimSVector <tim.schneider@vector.com>
2021-06-16 15:22:00 -0400

    adding in old style test case management report for squore

--- Start_VectorCAST.bat
+++ Start_VectorCAST.bat
@@ -4,6 +4,8 @@ rem set VECTORCAST_DIR=c:\vcast\2019sp2
 
 call setenv.bat
 
+set VCAST_REPORT_ENGINE=LEGACY
+
 pushd %CD%\CurrentRelease\vcast-workarea\vc_manage
 
 start /b %VECTORCAST_DIR%\vcastqt -lc -e PointOfSales_Manage
