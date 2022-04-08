e7a475d0cf57b515880187c5a4a9fdc18e1d9114
TimSVector <tim.schneider@vector.com>
2022-04-08 11:51:24 -0400

    Lint testing

--- CurrentRelease/lint.bat
+++ CurrentRelease/lint.bat
@@ -1,9 +1,9 @@
 @echo off
-pclp64 post.lnt %VCAST_DEMO_SRC_BASE%\database\src\database.c          
-pclp64 post.lnt %VCAST_DEMO_SRC_BASE%\encrypt\src\encrypt.c            
-pclp64 post.lnt %VCAST_DEMO_SRC_BASE%\encrypt\src\matrix_multiply.c    
-pclp64 post.lnt %VCAST_DEMO_SRC_BASE%\order_entry\src\manager.c          
-pclp64 post.lnt %VCAST_DEMO_SRC_BASE%\main\pos_driver.c            
-pclp64 post.lnt %VCAST_DEMO_SRC_BASE%\order_entry\src\waiting_list.c     
-pclp64 post.lnt %VCAST_DEMO_SRC_BASE%\utils\src\whitebox.c             
-pclp64 post.lnt %VCAST_DEMO_SRC_BASE%\utils\src\linked_list.c          
+pclp64 post.lnt database\src\database.c          
+pclp64 post.lnt encrypt\src\encrypt.c            
+pclp64 post.lnt encrypt\src\matrix_multiply.c    
+pclp64 post.lnt order_entry\src\manager.c          
+pclp64 post.lnt main\pos_driver.c            
+pclp64 post.lnt order_entry\src\waiting_list.c     
+pclp64 post.lnt utils\src\whitebox.c             
+pclp64 post.lnt utils\src\linked_list.c          
