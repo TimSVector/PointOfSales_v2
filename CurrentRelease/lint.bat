a49a089acbc7e3bcd8d6f489d710afd76077132b
TimSVector <tim.schneider@vector.com>
2022-04-08 10:23:14 -0400

    adding pclint+ info

---
+++ CurrentRelease/lint.bat
@@ -0,0 +1,9 @@
+@echo off
+pclp64 post.lnt database\src\database.c          
+pclp64 post.lnt encrypt\src\encrypt.c            
+pclp64 post.lnt encrypt\src\matrix_multiply.c    
+pclp64 post.lnt order_entry\src\manager.c          
+pclp64 post.lnt main\pos_driver.c            
+pclp64 post.lnt order_entry\src\waiting_list.c     
+pclp64 post.lnt utils\src\whitebox.c             
+pclp64 post.lnt utils\src\linked_list.c          
