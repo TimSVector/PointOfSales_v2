c0ab9061da9b7ada594b123b9e2c1c1538655f4f
TimSVector <tim.schneider@vector.com>
2022-10-31 16:22:08 -0400

    update for d: drive install of ada tools

--- setenv.bat
+++ setenv.bat
@@ -3,9 +3,22 @@ REM set PATH=c:\vcast\mingw\bin;%PATH%
 set VCAST_DEMO_SRC_BASE=%CD%\CurrentRelease
 
 rem Set path to gnat compiler to default path
-set path=%path%;c:\vector\tools\gnat\2017\bin
+set path=%path%;d:\vector\tools\gnat\2021\bin
 
 rem Set path to pclp64 to default path
-set PATH=%PATH%;C:\Vector\tools\pc-lint-plus\1.4.1\windows
+set PATH=%PATH%;d:\Vector\tools\pc-lint-plus\1.4.1\windows
 
-set VCAST_NUM_JOBS=8
+rem set VCAST_NUM_JOBS=8
+
+REM goto :END
+REM if EXECUTOR_NUMBER exists and hasn't already been appended to VECTORCAST_DIR...
+if "%EXECUTOR_NUMBER%" == "" goto :END
+if "%VECTORCAST_DIR:~-2,1%" == "@" goto :end
+
+
+set VECTORCAST_DIR=%VECTORCAST_DIR%@%EXECUTOR_NUMBER%
+
+
+:END
+
+echo %VECTORCAST_DIR%
