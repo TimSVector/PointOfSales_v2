000dd838c9b412dffac0fcaaa16356a2d54af8e3
TimSVector <tim.schneider@vector.com>
2023-03-13 11:27:29 -0400

    Update for path based code coverage

--- fast_test/tutorial/c/buildIt.bat
+++ fast_test/tutorial/c/buildIt.bat
@@ -1,8 +1,20 @@
+pushd %~dp0
+
 set path=%VECTORCAST_DIR%\mingw\bin;%PATH%
 if exist managerDriver.exe del managerDriver.exe
+if exist coverage.xml del coverage.xml
+del /q *.gcda
 
 rem This compile command builds an executable that needs stdin
-gcc -g manager_driver.c manager.c database.c whitebox.c -o managerDriver.exe || exit 1
+rem gcc -g manager_driver.c manager.c database.c whitebox.c -o managerDriver.exe || exit 1
 
 rem This compile command builds an executable that does not need stdin
-rem gcc -g -DORDER manager_driver.c manager.c database.c whitebox.c -o managerDriver.exe || exit 1
+gcc -g -DCHECK -fprofile-arcs -ftest-coverage -fPIC -O0 -I./inc order_entry/main/manager_driver.c order_entry/manager.c database/database.c utils/whitebox.c -o managerDriver.exe 
+
+managerDriver
+
+popd
+
+if "" == "%CI_PROJECT_DIR%" set CI_PROJECT_DIR=%cd%
+
+%VECTORCAST_DIR%\vpython %~dp0test.py --xml-pretty --exclude-unreachable-branches --print-summary -o coverage.xml --root %CI_PROJECT_DIR%
---
+++ fast_test/tutorial/c/generate_cobertura_coco.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import re
+import sys
+from gcovr.__main__ import main
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+    print(sys.argv[0])
+    
+    print(" ".join(sys.argv))
+    main()
+    xml_data = []
+    with open("coverage.xml","r") as fd:
+        xml_data = fd.readlines()
+        num = float(xml_data[2].split()[1].split("=")[-1].replace("\"","")) * 100.0
+        print ("coverage: " + str(num) + "% of statements")
+        xml_data[4] = "    <source>./</source>"
+                
+    with open("coverage2.xml","w") as fd:
+        fd.write("".join(xml_data))
+        
+    sys.exit()
