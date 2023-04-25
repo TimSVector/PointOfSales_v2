65d6f2994d0551e2608f4927cf988c7014682061
TimSVector <tim.schneider@vector.com>
2023-04-25 18:01:32 -0400

    Update for forloop

--- 2018_fast_test/test_versions.bat
+++ 2018_fast_test/test_versions.bat
@@ -1,74 +1,22 @@
 
 set ORIG_PATH=%PATH%
 
-set VECTORCAST_DIR=c:\vcast\2018sp5
-set path=%VECTORCAST_DIR%;%PATH%
-git clean -fxd
-git reset --hard HEAD
+for %%v in (2018sp5, 2019sp6, 2020sp7, 2021sp8, 2022sp8) do (
 
-manage -p 2018_fast_test --clean
-manage -p 2018_fast_test --build-execute > unstashed_build.log 
+    echo set VECTORCAST_DIR=c:\vcast\%%v
+    set path=%VECTORCAST_DIR%;%PATH%
+    git clean -fxd
+    git reset --hard HEAD
 
-%VECTORCAST_DIR%/vpython  "d:\dev\jenkins\remote\workspace\2018_fast_test_vcast_pipeline"/vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
-dir xml_data
-set PATH=%ORIG_PATH%
+    manage -p 2018_fast_test --clean
+    manage -p 2018_fast_test --build-execute > unstashed_build.log 
 
-pause
+    %VECTORCAST_DIR%/vpython  "d:\dev\jenkins\remote\workspace\2018_fast_test_vcast_pipeline"/vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
+    dir xml_data
+    set PATH=%ORIG_PATH%
 
+    pause
 
-set VECTORCAST_DIR=c:\vcast\2019sp6
-set path=%VECTORCAST_DIR%;%PATH%
-git clean -fxd
-git reset --hard HEAD
-
-manage -p 2018_fast_test --clean
-manage -p 2018_fast_test --build-execute > unstashed_build.log 
-
-%VECTORCAST_DIR%/vpython  "d:\dev\jenkins\remote\workspace\2018_fast_test_vcast_pipeline"/vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
-dir xml_data
-set PATH=%ORIG_PATH%
-
-pause
+)
 
 
-set VECTORCAST_DIR=c:\vcast\2020sp7
-set path=%VECTORCAST_DIR%;%PATH%
-git clean -fxd
-git reset --hard HEAD
-
-manage -p 2018_fast_test --clean
-manage -p 2018_fast_test --build-execute > unstashed_build.log 
-
-%VECTORCAST_DIR%/vpython  "d:\dev\jenkins\remote\workspace\2018_fast_test_vcast_pipeline"/vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
-dir xml_data
-set PATH=%ORIG_PATH%
-
-pause
-
-set VECTORCAST_DIR=c:\vcast\2021sp8
-set path=%VECTORCAST_DIR%;%PATH%
-git clean -fxd
-git reset --hard HEAD
-
-manage -p 2018_fast_test --clean
-manage -p 2018_fast_test --build-execute > unstashed_build.log 
-
-%VECTORCAST_DIR%/vpython  "d:\dev\jenkins\remote\workspace\2018_fast_test_vcast_pipeline"/vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
-dir xml_data
-set PATH=%ORIG_PATH%
-
-pause
-
-set VECTORCAST_DIR=c:\vcast\2022sp8
-set path=%VECTORCAST_DIR%;%PATH%
-git clean -fxd
-git reset --hard HEAD
-
-manage -p 2018_fast_test --clean
-manage -p 2018_fast_test --build-execute > unstashed_build.log 
-
-%VECTORCAST_DIR%/vpython  "d:\dev\jenkins\remote\workspace\2018_fast_test_vcast_pipeline"/vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
-dir xml_data
-set PATH=%ORIG_PATH%
-
-pause
