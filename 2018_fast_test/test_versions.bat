0295833a9df02bc124f93c46da2d9e78bd56d7b4
TimSVector <tim.schneider@vector.com>
2023-04-26 11:06:22 -0400

    updates for spf and imported results

--- 2018_fast_test/test_versions.bat
+++ 2018_fast_test/test_versions.bat
@@ -1,13 +1,40 @@
+@echo off
+set DO_SFP=
+set DO_IMPORT=
+
+if "%1"=="SFP" (
+set DO_SFP=1
+)
+if "%2"=="SFP" (
+set DO_SFP=1
+)
+if "%1"=="IMPORT" (
+set DO_IMPORT=1
+)
+if "%2"=="IMPORT" (
+set DO_IMPORT=1
+)
 
 set ORIG_PATH=%PATH%
 
+@echo on
 set VECTORCAST_DIR=c:\vcast\2018sp5
 set path=%VECTORCAST_DIR%;%PATH%
 git clean -fxd
 git reset --hard HEAD
 
 manage -p 2018_fast_test --clean
+
+if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
+
 manage -p 2018_fast_test --build-execute > unstashed_build.log 
+
+if "%DO_IMPORT%" == "1" (
+    manage -p 2018_fast_test --export-result temp_result.cvr
+    manage -p 2018_fast_test --clean
+    manage -p 2018_fast_test --import-result temp_result.cvr
+    manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
+)
 
 %VECTORCAST_DIR%/vpython  "d:\dev\jenkins\remote\workspace\2018_fast_test_vcast_pipeline"/vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
 dir xml_data
@@ -22,6 +49,7 @@ git clean -fxd
 git reset --hard HEAD
 
 manage -p 2018_fast_test --clean
+if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
 manage -p 2018_fast_test --build-execute > unstashed_build.log 
 
 %VECTORCAST_DIR%/vpython  "d:\dev\jenkins\remote\workspace\2018_fast_test_vcast_pipeline"/vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
@@ -37,7 +65,15 @@ git clean -fxd
 git reset --hard HEAD
 
 manage -p 2018_fast_test --clean
+if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
 manage -p 2018_fast_test --build-execute > unstashed_build.log 
+
+if "%DO_IMPORT%" == "1" (
+    manage -p 2018_fast_test --export-result temp_result.cvr
+    manage -p 2018_fast_test --clean
+    manage -p 2018_fast_test --import-result temp_result.cvr
+    manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
+) 
 
 %VECTORCAST_DIR%/vpython  "d:\dev\jenkins\remote\workspace\2018_fast_test_vcast_pipeline"/vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
 dir xml_data
@@ -51,7 +87,15 @@ git clean -fxd
 git reset --hard HEAD
 
 manage -p 2018_fast_test --clean
+if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
 manage -p 2018_fast_test --build-execute > unstashed_build.log 
+
+if "%DO_IMPORT%" == "1" (
+    manage -p 2018_fast_test --export-result temp_result.cvr
+    manage -p 2018_fast_test --clean
+    manage -p 2018_fast_test --import-result temp_result.cvr
+    manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
+) 
 
 %VECTORCAST_DIR%/vpython  "d:\dev\jenkins\remote\workspace\2018_fast_test_vcast_pipeline"/vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
 dir xml_data
@@ -65,7 +109,15 @@ git clean -fxd
 git reset --hard HEAD
 
 manage -p 2018_fast_test --clean
+if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
 manage -p 2018_fast_test --build-execute > unstashed_build.log 
+
+if "%DO_IMPORT%" == "1" (
+    manage -p 2018_fast_test --export-result temp_result.cvr
+    manage -p 2018_fast_test --clean
+    manage -p 2018_fast_test --import-result temp_result.cvr
+    manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
+) 
 
 %VECTORCAST_DIR%/vpython  "d:\dev\jenkins\remote\workspace\2018_fast_test_vcast_pipeline"/vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
 dir xml_data
