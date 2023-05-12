82395a3124aac9a948d23231945d5736a598035e
TimSVector <tim.schneider@vector.com>
2023-05-12 11:10:32 -0400

    adding 2023 tests

--- 2018_fast_test/test_versions.bat
+++ 2018_fast_test/test_versions.bat
@@ -29,6 +29,7 @@ xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* vc_scripts > nul
 
 manage -p 2018_fast_test --clean
 if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
+%VECTORCAST_DIR%/vpython  vc_scripts/getjobs.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --type
 manage -p 2018_fast_test --build-execute > unstashed_build.log 
 
 if "%DO_IMPORT%" == "1" (
@@ -43,7 +44,7 @@ dir xml_data
 set PATH=%ORIG_PATH%
 set VECTORCAST_DIR=%ORIG_VCD%
 
-pause
+::pause
 
 set VECTORCAST_DIR=c:\vcast\2019sp6
 set path=%VECTORCAST_DIR%;%PATH%
@@ -53,6 +54,7 @@ xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* vc_scripts > nul
 
 manage -p 2018_fast_test --clean
 if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
+%VECTORCAST_DIR%/vpython  vc_scripts/getjobs.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --type
 manage -p 2018_fast_test --build-execute > unstashed_build.log 
 
 if "%DO_IMPORT%" == "1" (
@@ -67,7 +69,7 @@ dir xml_data
 set PATH=%ORIG_PATH%
 set VECTORCAST_DIR=%ORIG_VCD%
 
-pause
+::pause
 
 set VECTORCAST_DIR=c:\vcast\2020sp7
 set path=%VECTORCAST_DIR%;%PATH%
@@ -77,6 +79,7 @@ xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* vc_scripts > nul
 
 manage -p 2018_fast_test --clean
 if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
+%VECTORCAST_DIR%/vpython  vc_scripts/getjobs.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --type
 manage -p 2018_fast_test --build-execute > unstashed_build.log 
 
 if "%DO_IMPORT%" == "1" (
@@ -91,7 +94,7 @@ dir xml_data
 set PATH=%ORIG_PATH%
 set VECTORCAST_DIR=%ORIG_VCD%
 
-pause
+::pause
 
 set VECTORCAST_DIR=c:\vcast\2021sp8
 set path=%VECTORCAST_DIR%;%PATH%
@@ -101,6 +104,7 @@ xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* vc_scripts > nul
 
 manage -p 2018_fast_test --clean
 if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
+%VECTORCAST_DIR%/vpython  vc_scripts/getjobs.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --type
 manage -p 2018_fast_test --build-execute > unstashed_build.log 
 
 if "%DO_IMPORT%" == "1" (
@@ -115,7 +119,7 @@ dir xml_data
 set PATH=%ORIG_PATH%
 set VECTORCAST_DIR=%ORIG_VCD%
 
-pause
+::pause
 
 set VECTORCAST_DIR=c:\vcast\2022sp8
 set path=%VECTORCAST_DIR%;%PATH%
@@ -125,6 +129,7 @@ xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* vc_scripts > nul
 
 manage -p 2018_fast_test --clean
 if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
+%VECTORCAST_DIR%/vpython  vc_scripts/getjobs.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --type
 manage -p 2018_fast_test --build-execute > unstashed_build.log 
 
 if "%DO_IMPORT%" == "1" (
@@ -139,4 +144,30 @@ dir xml_data
 set PATH=%ORIG_PATH%
 set VECTORCAST_DIR=%ORIG_VCD%
 
-pause
+::pause
+
+
+set VECTORCAST_DIR=c:\vcast\2023
+set path=%VECTORCAST_DIR%;%PATH%
+git clean -fxd
+git reset --hard HEAD
+xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* vc_scripts > nul
+
+manage -p 2018_fast_test --clean
+if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
+%VECTORCAST_DIR%/vpython  vc_scripts/getjobs.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --type
+manage -p 2018_fast_test --build-execute > unstashed_build.log 
+
+if "%DO_IMPORT%" == "1" (
+    manage -p 2018_fast_test --export-result temp_result.vcr
+    manage -p 2018_fast_test --clean
+    manage -p 2018_fast_test --import-result temp_result.vcr
+    manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
+) 
+
+%VECTORCAST_DIR%/vpython  vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
+dir xml_data
+set PATH=%ORIG_PATH%
+set VECTORCAST_DIR=%ORIG_VCD%
+
+::pause
