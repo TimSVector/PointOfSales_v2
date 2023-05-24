1c7c9f51e5423270cb41f11eadab4f806d5f4231
TimSVector <tim.schneider@vector.com>
2023-05-24 15:15:20 -0400

    updatge for full path to manage

--- 2018_fast_test/test_versions.bat
+++ 2018_fast_test/test_versions.bat
@@ -27,16 +27,16 @@ git clean -fxd
 git reset --hard HEAD
 xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* vc_scripts > nul
 
-manage -p 2018_fast_test --clean
-if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
+%VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+if "%DO_SFP%" == "1" %VECTORCAST_DIR%\manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
 %VECTORCAST_DIR%/vpython  vc_scripts/getjobs.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --type
-manage -p 2018_fast_test --build-execute > unstashed_build.log 
+%VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute > unstashed_build.log 
 
 if "%DO_IMPORT%" == "1" (
-    manage -p 2018_fast_test --export-result temp_result.vcr
-    manage -p 2018_fast_test --clean
-    manage -p 2018_fast_test --import-result temp_result.vcr
-    manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --export-result temp_result.vcr
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --import-result temp_result.vcr
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
 )
 
 %VECTORCAST_DIR%/vpython  vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
@@ -52,16 +52,16 @@ git clean -fxd
 git reset --hard HEAD
 xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* vc_scripts > nul
 
-manage -p 2018_fast_test --clean
-if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
+%VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+if "%DO_SFP%" == "1" %VECTORCAST_DIR%\manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
 %VECTORCAST_DIR%/vpython  vc_scripts/getjobs.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --type
-manage -p 2018_fast_test --build-execute > unstashed_build.log 
+%VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute > unstashed_build.log 
 
 if "%DO_IMPORT%" == "1" (
-    manage -p 2018_fast_test --export-result temp_result.vcr
-    manage -p 2018_fast_test --clean
-    manage -p 2018_fast_test --import-result temp_result.vcr
-    manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --export-result temp_result.vcr
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --import-result temp_result.vcr
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
 )
 
 %VECTORCAST_DIR%/vpython  vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
@@ -77,16 +77,16 @@ git clean -fxd
 git reset --hard HEAD
 xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* vc_scripts > nul
 
-manage -p 2018_fast_test --clean
-if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
+%VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+if "%DO_SFP%" == "1" %VECTORCAST_DIR%\manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
 %VECTORCAST_DIR%/vpython  vc_scripts/getjobs.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --type
-manage -p 2018_fast_test --build-execute > unstashed_build.log 
+%VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute > unstashed_build.log 
 
 if "%DO_IMPORT%" == "1" (
-    manage -p 2018_fast_test --export-result temp_result.vcr
-    manage -p 2018_fast_test --clean
-    manage -p 2018_fast_test --import-result temp_result.vcr
-    manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --export-result temp_result.vcr
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --import-result temp_result.vcr
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
 ) 
 
 %VECTORCAST_DIR%/vpython  vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
@@ -102,16 +102,16 @@ git clean -fxd
 git reset --hard HEAD
 xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* vc_scripts > nul
 
-manage -p 2018_fast_test --clean
-if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
+%VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+if "%DO_SFP%" == "1" %VECTORCAST_DIR%\manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
 %VECTORCAST_DIR%/vpython  vc_scripts/getjobs.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --type
-manage -p 2018_fast_test --build-execute > unstashed_build.log 
+%VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute > unstashed_build.log 
 
 if "%DO_IMPORT%" == "1" (
-    manage -p 2018_fast_test --export-result temp_result.vcr
-    manage -p 2018_fast_test --clean
-    manage -p 2018_fast_test --import-result temp_result.vcr
-    manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --export-result temp_result.vcr
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --import-result temp_result.vcr
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
 ) 
 
 %VECTORCAST_DIR%/vpython  vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
@@ -127,16 +127,16 @@ git clean -fxd
 git reset --hard HEAD
 xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* vc_scripts > nul
 
-manage -p 2018_fast_test --clean
-if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
+%VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+if "%DO_SFP%" == "1" %VECTORCAST_DIR%\manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
 %VECTORCAST_DIR%/vpython  vc_scripts/getjobs.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --type
-manage -p 2018_fast_test --build-execute > unstashed_build.log 
+%VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute > unstashed_build.log 
 
 if "%DO_IMPORT%" == "1" (
-    manage -p 2018_fast_test --export-result temp_result.vcr
-    manage -p 2018_fast_test --clean
-    manage -p 2018_fast_test --import-result temp_result.vcr
-    manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --export-result temp_result.vcr
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --import-result temp_result.vcr
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
 ) 
 
 %VECTORCAST_DIR%/vpython  vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
@@ -153,16 +153,16 @@ git clean -fxd
 git reset --hard HEAD
 xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* vc_scripts > nul
 
-manage -p 2018_fast_test --clean
-if "%DO_SFP%" == "1" manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
+%VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+if "%DO_SFP%" == "1" %VECTORCAST_DIR%\manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE
 %VECTORCAST_DIR%/vpython  vc_scripts/getjobs.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --type
-manage -p 2018_fast_test --build-execute > unstashed_build.log 
+%VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute > unstashed_build.log 
 
 if "%DO_IMPORT%" == "1" (
-    manage -p 2018_fast_test --export-result temp_result.vcr
-    manage -p 2018_fast_test --clean
-    manage -p 2018_fast_test --import-result temp_result.vcr
-    manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --export-result temp_result.vcr
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --import-result temp_result.vcr
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
 ) 
 
 %VECTORCAST_DIR%/vpython  vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
