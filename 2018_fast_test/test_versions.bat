848eafeb1b2a62df713528b2dde458dcc138ed40
TimSVector <tim.schneider@vector.com>
2023-05-25 10:13:31 -0400

    updates for CBT test

--- 2018_fast_test/test_versions.bat
+++ 2018_fast_test/test_versions.bat
@@ -32,8 +32,9 @@ if "%DO_IMPORT%" == "1" (
     %VECTORCAST_DIR%\manage -p 2018_fast_test --export-result temp_result.vcr
     %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
     %VECTORCAST_DIR%\manage -p 2018_fast_test --import-result temp_result.vcr
-    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log & type unstashed_build.log
 )
+
+%VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log & type unstashed_build.log
 
 %VECTORCAST_DIR%/vpython  vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
 dir xml_data
@@ -58,8 +59,9 @@ if "%DO_IMPORT%" == "1" (
     %VECTORCAST_DIR%\manage -p 2018_fast_test --export-result temp_result.vcr
     %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
     %VECTORCAST_DIR%\manage -p 2018_fast_test --import-result temp_result.vcr
-    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log & type unstashed_build.log
 )
+
+%VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log & type unstashed_build.log
 
 %VECTORCAST_DIR%/vpython  vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
 dir xml_data
@@ -84,8 +86,9 @@ if "%DO_IMPORT%" == "1" (
     %VECTORCAST_DIR%\manage -p 2018_fast_test --export-result temp_result.vcr
     %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
     %VECTORCAST_DIR%\manage -p 2018_fast_test --import-result temp_result.vcr
-    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log & type unstashed_build.log
 ) 
+
+%VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log & type unstashed_build.log
 
 %VECTORCAST_DIR%/vpython  vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
 dir xml_data
@@ -110,8 +113,9 @@ if "%DO_IMPORT%" == "1" (
     %VECTORCAST_DIR%\manage -p 2018_fast_test --export-result temp_result.vcr
     %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
     %VECTORCAST_DIR%\manage -p 2018_fast_test --import-result temp_result.vcr
-    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log & type unstashed_build.log
 ) 
+
+%VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log & type unstashed_build.log
 
 %VECTORCAST_DIR%/vpython  vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
 dir xml_data
@@ -136,8 +140,9 @@ if "%DO_IMPORT%" == "1" (
     %VECTORCAST_DIR%\manage -p 2018_fast_test --export-result temp_result.vcr
     %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
     %VECTORCAST_DIR%\manage -p 2018_fast_test --import-result temp_result.vcr
-    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log & type unstashed_build.log
 ) 
+
+%VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log & type unstashed_build.log
 
 %VECTORCAST_DIR%/vpython  vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
 dir xml_data
@@ -145,7 +150,6 @@ set PATH=%ORIG_PATH%
 set VECTORCAST_DIR=%ORIG_VCD%
 
 ::pause
-
 
 set VECTORCAST_DIR=c:\vcast\2023
 git clean -fxd
@@ -163,8 +167,9 @@ if "%DO_IMPORT%" == "1" (
     %VECTORCAST_DIR%\manage -p 2018_fast_test --export-result temp_result.vcr
     %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
     %VECTORCAST_DIR%\manage -p 2018_fast_test --import-result temp_result.vcr
-    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log & type unstashed_build.log
 ) 
+
+%VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log & type unstashed_build.log
 
 %VECTORCAST_DIR%/vpython  vc_scripts/generate-results.py  D:/dev/PointOfSales_v2/2018_fast_test/2018_fast_test.vcm --wait_time 30 --wait_loops 1 --junit --buildlog unstashed_build.log --print_exc
 dir xml_data
