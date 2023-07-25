1975d8ae8f025bc91e54646acbeb852e625bc091
TimSVector <tim.schneider@vector.com>
2023-07-25 09:26:27 -0400

    adding run_version_tests.bat

---
+++ run_version_tests.bat
@@ -0,0 +1,21 @@
+
+
+cd 2018_fast_test
+
+call test_versions.bat
+call test_versions.bat                   DO_MERGE
+call test_versions.bat            MODIFY         
+call test_versions.bat            MODIFY DO_MERGE
+call test_versions.bat     IMPORT
+call test_versions.bat     IMPORT        DO_MERGE
+call test_versions.bat     IMPORT MODIFY 
+call test_versions.bat     IMPORT MODIFY DO_MERGE
+call test_versions.bat SFP
+call test_versions.bat SFP               DO_MERGE
+call test_versions.bat SFP        MODIFY         
+call test_versions.bat SFP        MODIFY DO_MERGE
+call test_versions.bat SFP IMPORT
+call test_versions.bat SFP IMPORT        DO_MERGE
+call test_versions.bat SFP IMPORT MODIFY 
+call test_versions.bat SFP IMPORT MODIFY DO_MERGE
+
