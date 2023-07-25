253f2b3a79e27e805fcff3f48b4f0f72eaa25b35
TimSVector <tim.schneider@vector.com>
2023-07-25 09:28:13 -0400

    add testing bats to CurrentRelease/vcast-workarea/vc_manage workflow to pick up the system test info

---
+++ CurrentRelease/vcast-workarea/vc_manage/test_versions.bat
@@ -0,0 +1,32 @@
+set DO_SFP=0
+set DO_IMPORT=0
+set DO_MODIFY=0
+set DO_MERGE=0
+
+echo %*
+
+echo %* | findstr /i SFP    > nul
+if "%errorlevel%"=="0" set DO_SFP=1
+echo %* | findstr /i IMPORT    > nul
+if "%errorlevel%"=="0" set DO_IMPORT=1
+echo %* | findstr /i MODIFY    > nul
+if "%errorlevel%"=="0" set DO_MODIFY=1
+echo %* | findstr /i MERGE    > nul
+if "%errorlevel%"=="0" set DO_MERGE=1
+
+set workspace=%cd%
+
+set ORIG_VCD=%VECTORCAST_DIR%
+
+@echo on
+call single_test.bat c:\vcast\2018sp5 %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE%
+call single_test.bat c:\vcast\2019sp6 %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE%
+call single_test.bat c:\vcast\2020sp7 %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE%
+call single_test.bat c:\vcast\2021sp8 %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE%
+call single_test.bat c:\vcast\2022sp8 %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE%
+call single_test.bat c:\vcast\2023sp2 %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE%
+
+set VECTORCAST_DIR=%ORIG_VCD%
+
+:END
+
