50be0e85ec4cbbcb44640110ef8434eda16027bf
TimSVector <tim.schneider@vector.com>
2023-08-25 12:51:26 -0400

    Update to remove merge without import

--- run_version_tests.bat
+++ run_version_tests.bat
@@ -3,17 +3,13 @@
 pushd 2018_fast_test
 
 call test_versions.bat
-call test_versions.bat                   DO_MERGE
 call test_versions.bat            MODIFY         
-call test_versions.bat            MODIFY DO_MERGE
 call test_versions.bat     IMPORT
 call test_versions.bat     IMPORT        DO_MERGE
 call test_versions.bat     IMPORT MODIFY 
 call test_versions.bat     IMPORT MODIFY DO_MERGE
 call test_versions.bat SFP
-call test_versions.bat SFP               DO_MERGE
 call test_versions.bat SFP        MODIFY         
-call test_versions.bat SFP        MODIFY DO_MERGE
 call test_versions.bat SFP IMPORT
 call test_versions.bat SFP IMPORT        DO_MERGE
 call test_versions.bat SFP IMPORT MODIFY 
@@ -24,17 +20,13 @@ call setenv.bat
 pushd CurrentRelease\vcast-workarea\vc_manage
 
 call test_versions.bat
-call test_versions.bat                   DO_MERGE
 call test_versions.bat            MODIFY         
-call test_versions.bat            MODIFY DO_MERGE
 call test_versions.bat     IMPORT
 call test_versions.bat     IMPORT        DO_MERGE
 call test_versions.bat     IMPORT MODIFY 
 call test_versions.bat     IMPORT MODIFY DO_MERGE
 call test_versions.bat SFP
-call test_versions.bat SFP               DO_MERGE
 call test_versions.bat SFP        MODIFY         
-call test_versions.bat SFP        MODIFY DO_MERGE
 call test_versions.bat SFP IMPORT
 call test_versions.bat SFP IMPORT        DO_MERGE
 call test_versions.bat SFP IMPORT MODIFY 
