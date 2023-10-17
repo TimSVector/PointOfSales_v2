601ed63be4a42760eea23a716c3533812352341f
TimSVector <tim.schneider@vector.com>
2023-10-17 09:45:32 -0400

    updates

---
+++ CurrentRelease/test-data/test.cpp
@@ -0,0 +1,14 @@
+void encrypt_transmit_Info_failure(void) {}
+void encrypt_transmit_Info_good(void) {}
+void BASIS_PATH_001(void) {}
+void BASIS_PATH_002(void) {}
+void manager_add_included_dessert_cake(void) {}
+void manager_add_included_dessert_none(void) {}
+void manager_add_included_dessert_null(void) {}
+void manager_add_included_dessert_pie(void) {}
+void manager_Place_Order_All_Entrees(void) {}
+void manager_Place_Order_SeatCount(void) {}
+void manager_Place_Order_TableCount(void) {}
+void manager_Place_Order_Fish(void) {}
+
+// D:\dev\PointOfSales_v2\xml_data\sonarqube\test.cpp
--- setenv.bat
+++ setenv.bat
@@ -8,7 +8,7 @@ rem set path=%path%;d:\vector\tools\gnat\2021\bin
 rem Set path to pclp64 to default path
 rem set PATH=%PATH%;d:\Vector\tools\pc-lint-plus\1.4.1\windows;C:\VCAST\Vector.TestInsights.Cli.1.0.125+109
 
-set path=%path%;%VECTOCAST_DIR%\mingw\bin
+set path=%path%;%VECTORCAST_DIR%\mingw\bin
 
 set SQ_INSTALL_DIR=D:\vector\tools\sonarqube-10.2.1.78527\bin\windows-x86-64
 set path=%SQ_INSTALL_DIR%\jdk-17.0.8.1\bin;%SQ_INSTALL_DIR%\build-wrapper-win-x86;%SQ_INSTALL_DIR%\sonar-scanner-5.0.1.3006-windows\bin;%PATH%
