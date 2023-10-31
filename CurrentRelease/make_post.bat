8480206d75642244d2d9b9d88725878ff2d565c0
TimSVector <tim.schneider@vector.com>
2023-10-31 15:34:32 -0400

    update

--- CurrentRelease/make_post.bat
+++ CurrentRelease/make_post.bat
@@ -1,4 +1,4 @@
-rem @echo off
+@echo off
 pushd %~dp0
 call ..\setenv.bat
 
@@ -33,7 +33,6 @@ gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -
 gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\waiting_list.o    -c %MGR_DIR%\src\waiting_list.c     >> build.log 2>&1
 gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\whitebox.o        -c utils\src\whitebox.c             >> build.log 2>&1
 gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\linked_list.o     -c utils\src\linked_list.c          >> build.log 2>&1
-gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\test.o            -c test-data\test.cpp               >> build.log 2>&1
 
 gcc build/*.o -o %EXEC% >> build.log 2>&1
 @echo Completed Make >> build.log
