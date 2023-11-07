a115769caa54092f7fd53ea006e1936110d7732b
TimSVector <tim.schneider@vector.com>
2023-11-07 17:50:48 -0500

    Update to do python instead of silly batch files

---
+++ run_version_tests.py
@@ -0,0 +1,39 @@
+#run_versions_tests.py
+
+import os
+import subprocess
+
+cli_args = ["                          ",
+            "            MODIFY        ",  
+            "    IMPORT                ", 
+            "    IMPORT        DO_MERGE", 
+            "    IMPORT MODIFY         ", 
+            "    IMPORT MODIFY DO_MERGE", 
+            "SFP                       ", 
+            "SFP        MODIFY         ", 
+            "SFP IMPORT                ", 
+            "SFP IMPORT        DO_MERGE", 
+            "SFP IMPORT MODIFY         ", 
+            "SFP IMPORT MODIFY DO_MERGE"] 
+            
+coverage_types = ["STATEMENT+MC/DC ","STATEMENT+BRANCH ","FUNCTION+FUNCTION_CALL","FUNCTION","MC/DC","BRANCH","STATEMENT"]
+
+directories = ["2018_fast_test", "CurrentRelease/vcast-workarea/vc_manage"]
+
+orig_dir = os.getcwd()
+
+for directory in directories:
+    os.chdir(orig_dir)
+    os.chdir(directory)
+    
+    for coverage_type in coverage_types:
+        os.environ['VCAST_CODE_COVERAGE_TYPE'] = coverage_type
+        for args in cli_args:
+            callCmd = ["test_versions.bat"] + args.split()
+            print(directory, coverage_type, " ".join(callCmd))
+            p = subprocess.Popen(callCmd, universal_newlines=True)
+            p.wait()
+
+            
+    
+    
