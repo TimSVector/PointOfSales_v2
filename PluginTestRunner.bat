c75df9d0f150f67c3f0654896e7c4dd7f21d5119
TimSVector <tim.schneider@vector.com>
2023-11-07 18:32:06 -0500

    updates

--- PluginTestRunner.bat
+++ PluginTestRunner.bat
@@ -7,6 +7,8 @@ xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* vc_scripts > nul
 set path=%PATH%;C:\vector\tools\gnat\2019\bin
 set WORKSPACE=%CD%
 
+%VECTORCAST_DIR%/manage --project PluginTesting.vcm --status
+%VECTORCAST_DIR%/manage --project PluginTesting.vcm --clean
 %VECTORCAST_DIR%/manage --project PluginTesting.vcm --build-execute --incremental --output PluginTesting.vcm_RebuildReport.html > build.log
 %VECTORCAST_DIR%/vpython vc_scripts/generate-results.py PluginTesting.vcm --junit --buildlog build.log
 %VECTORCAST_DIR%/manage --project PluginTesting.vcm --create-report=metrics --output=MetricsReport.html
