c56db659a727bc1c2e4dbc69d696ec9fe1121d64
TimSVector <tim.schneider@vector.com>
2023-11-08 17:26:35 -0500

    updates

--- PluginTestRunner.bat
+++ PluginTestRunner.bat
@@ -7,9 +7,16 @@ xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* vc_scripts > nul
 set path=%PATH%;C:\vector\tools\gnat\2019\bin
 set WORKSPACE=%CD%
 
-%VECTORCAST_DIR%/manage --project PluginTesting.vcm --status
-%VECTORCAST_DIR%/manage --project PluginTesting.vcm --clean
-%VECTORCAST_DIR%/manage --project PluginTesting.vcm --build-execute --incremental --output PluginTesting.vcm_RebuildReport.html > build.log
+%VECTORCAST_DIR%/manage  --project PluginTesting.vcm --status
+%VECTORCAST_DIR%/manage  --project PluginTesting.vcm --clean
+%VECTORCAST_DIR%/manage  --project PluginTesting.vcm --build-execute --incremental --output PluginTesting.vcm_RebuildReport.html > build.log
 %VECTORCAST_DIR%/vpython vc_scripts/generate-results.py PluginTesting.vcm --junit --buildlog build.log
-%VECTORCAST_DIR%/manage --project PluginTesting.vcm --create-report=metrics --output=MetricsReport.html
-%VECTORCAST_DIR%/manage --project PluginTesting.vcm --full-status=FullStatusReport.html
+%VECTORCAST_DIR%/vpython incremental_build_report_aggregator.py PluginTesting  --rptfmt HTML
+%VECTORCAST_DIR%/vpython vc_scripts/full_report_no_toc.py PluginTesting.vcm
+%VECTORCAST_DIR%/manage  --project PluginTesting.vcm --create-report=aggregate --output=MetricsReport.html
+%VECTORCAST_DIR%/manage  --project PluginTesting.vcm --create-report=environment --output=EnvironmentReport.html
+%VECTORCAST_DIR%/manage  --project PluginTesting.vcm --full-status=FullStatusReport.html
+
+dir *.html
+dir *tmp
+
