d2fb344aac307f6e0475cc5d3c5e7121a4bdb05d
TimSVector <tim.schneider@vector.com>
2023-11-16 09:16:44 -0500

    updates

--- run_version_tests.py
+++ run_version_tests.py
@@ -12,13 +12,13 @@ def parse_args():
     parser.add_argument('-m', '--post',   help='Run the tests on the PoST project',           action="store_true", dest="run_post", default=False)
     parser.add_argument('-p', '--plugin', help='Run the tests on the PluginTesting',          action="store_true", dest="run_plgn", default=False)
     parser.add_argument('-a', '--all',    help='Run all the tests',                           action="store_true", dest="run_all", default=False)
-    parser.add_argument('--vc_version',   help='Run specified test for specifc VC version',   action="store", dest="vc_version", default="")
+    parser.add_argument('--vc_version',   help='Run specified test for specifc VC version',   action="store", dest="vc_version", default=None)
 
     args = parser.parse_args()
 
     if args.vc_version and os.path.exists("c:/vcast/"+args.vc_version):
         os.environ['VC_VERSION'] = args.vc_version
-    else:
+    elif args.vc_version:
         print("VC Version: " + args.vc_version + " does not exist. Exiting")
         sys.exit()
 
@@ -26,6 +26,9 @@ def parse_args():
     return args
     
 def run_2018_post(args):
+
+    dt2018 = datetime.now()
+    dtPost = datetime.now()
         
     cli_args = ["                          ",
                 "            MODIFY        ",  
@@ -54,43 +57,52 @@ def run_2018_post(args):
             pass
             
         elif args.run_post and directory.startswith("CurrentRelease"):
+            dtPost = datetime.now()
             pass
         else:
-            print("2.1 Skipping " + directory + " test")
             continue
             
-        print("2. run_version_tests", directory, "start time", datetime.now())
         os.chdir(directory)
         
         for coverage_type in coverage_types:
             os.environ['VCAST_CODE_COVERAGE_TYPE'] = coverage_type
             for cargs in cli_args:
                 callCmd = ["test_versions.bat"] + cargs.split()
-                print("2.2", directory, coverage_type, " ".join(callCmd))
                 p = subprocess.Popen(callCmd, universal_newlines=True)
                 p.wait()
         os.chdir(orig_dir)
+        
+    return dt2018, dtPost
 
 def run_plugin(args):
 
     ## Additional tests -- plugin testing
-    print("3. run_version_tests PluginTestRunner start time", datetime.now())
-
     if args.run_all or args.run_plgn:
-        for vcd in [r'C:\VCAST\2022sp8', r'C:\VCAST\2023sp4']:
-            print("3.1 run_version_tests PluginTestRunner " + vcd + " start time", datetime.now())
+        for vcd in [r'C:\VCAST\2023sp4']:
             os.environ['VECTORCAST_DIR'] = vcd
             p = subprocess.Popen(["PluginTestRunner.bat"], universal_newlines=True)
             p.wait()
-    else:
-        print("3.2 Skipping PluginTestRunner.bat")
     
 if __name__ == '__main__':
 
-    print("1. run_version_tests start time", datetime.now())
+    startDT = datetime.now()
+
     args = parse_args()
     
-    run_2018_post(args)
+    dt2018, dtPost = run_2018_post(args)
+    
+    pluginDT = datetime.now()
+    
     run_plugin(args)
     
-    print("4. run_version_tests end time", datetime.now())
+    endDT = datetime.now()
+    
+    print("Start : ", startDT)
+    print("2018  : ", dt2018,   dtPost-dt2018)
+    print("PoST  : ", dtPost,   pluginDT-dtPost)
+    print("Plugin: ", pluginDT, endDT-pluginDT)
+    print("End   : ", endDT, endDT-startDT)
+    
+    
+    
+    
