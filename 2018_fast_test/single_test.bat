7ee5f2c79ef4f1e201227679d53a869c34655515
TimSVector <tim.schneider@vector.com>
2024-09-20 10:22:39 -0400

    updates for copy/extract test

--- 2018_fast_test/single_test.bat
+++ 2018_fast_test/single_test.bat
@@ -4,10 +4,12 @@ set DO_SFP=%2
 set DO_IMPORT=%3
 set DO_MODIFY=%4
 set DO_MERGE=%5
+set DO_COPY_EXTRACT=%5
 
 git clean -fxd 
 git reset --hard HEAD
 xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* vc_scripts > nul
+xcopy /E /S /Y /I %VCAST_VC_SCRIPTS_DONT_USE%\*.* vc_scripts > nul
 
 %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
 %VECTORCAST_DIR%\manage -p 2018_fast_test --config=COVERAGE_TYPE=%VCAST_CODE_COVERAGE_TYPE%
@@ -49,6 +51,34 @@ if "%DO_MERGE%"=="1" (
     :: 3rd build-execute with no changes - should only build system tests
     :: %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log & type unstashed_build.log
     %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --incremental > unstashed_build.log
+)
+
+if "%DO_COPY_EXTRACT%"=="1" (
+
+    pushd ..
+    set WORKSPACE=%cd%
+    popd
+
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --level VectorCAST_MinGW_C/TestSuite --environment DATABASE_C
+    %VECTORCAST_DIR%\vpython  vc_scripts\copy_build_dir.py 2018_fast_test.vcm VectorCAST_MinGW_C/TestSuite 2018_fast_test_VectorCAST_MinGW_C_TestSuite_DATABASE_C DATABASE_C
+
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --level VectorCAST_MinGW_C/TestSuite --environment MANAGER_C
+    %VECTORCAST_DIR%\vpython  vc_scripts\copy_build_dir.py 2018_fast_test.vcm VectorCAST_MinGW_C/TestSuite 2018_fast_test_VectorCAST_MinGW_C_TestSuite_MANAGER_C MANAGER_C
+
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --level VectorCAST_MinGW_C/TestSuite2 --environment DATABASE_C
+    %VECTORCAST_DIR%\vpython  vc_scripts\copy_build_dir.py 2018_fast_test.vcm VectorCAST_MinGW_C/TestSuite2 2018_fast_test_VectorCAST_MinGW_C_TestSuite2_DATABASE_C DATABASE_C
+
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --build-execute --level VectorCAST_MinGW_C/TestSuite2 --environment INTEGRATION_TEST
+    %VECTORCAST_DIR%\vpython  vc_scripts\copy_build_dir.py 2018_fast_test.vcm VectorCAST_MinGW_C/TestSuite2 2018_fast_test_VectorCAST_MinGW_C_TestSuite2_INTEGRATION_TEST INTEGRATION_TEST
+
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --clean
+    %VECTORCAST_DIR%\vpython  vc_scripts\extract_build_dir.py leaveFiles
+
+    %VECTORCAST_DIR%\manage -p 2018_fast_test --full-status=copy_extract_full_status.html
 )
 
 :END
--- 2018_fast_test/test_versions.bat
+++ 2018_fast_test/test_versions.bat
@@ -2,6 +2,7 @@ set DO_SFP=0
 set DO_IMPORT=0
 set DO_MODIFY=0
 set DO_MERGE=0
+set DO_COPY_EXTRACT=0
 
 echo %*
 
@@ -13,6 +14,8 @@ echo %* | findstr /i MODIFY    > nul
 if "%errorlevel%"=="0" set DO_MODIFY=1
 echo %* | findstr /i MERGE    > nul
 if "%errorlevel%"=="0" set DO_MERGE=1
+echo %* | findstr /i COPY_EXTRACT  > nul
+if "%errorlevel%"=="0" set DO_COPY_EXTRACT=1
 
 set workspace=%cd%
 
@@ -22,13 +25,13 @@ set ORIG_VCD=%VECTORCAST_DIR%
 
 if "%VC_VERSION%"=="" (
 
-    call single_test.bat c:\vcast\2018sp5 %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE%
-    call single_test.bat c:\vcast\2019sp6 %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE%
-    call single_test.bat c:\vcast\2020sp7 %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE%
-    call single_test.bat c:\vcast\2023sp5 %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE%
+    call single_test.bat c:\vcast\2018sp5 %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE% %DO_COPY_EXTRACT%
+    call single_test.bat c:\vcast\2019sp6 %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE% %DO_COPY_EXTRACT%
+    call single_test.bat c:\vcast\2020sp7 %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE% %DO_COPY_EXTRACT%
+    call single_test.bat c:\vcast\2023sp5 %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE% %DO_COPY_EXTRACT%
 
 ) else (
-    call single_test.bat c:\vcast\%VC_VERSION% %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE%
+    call single_test.bat c:\vcast\%VC_VERSION% %DO_SFP% %DO_IMPORT% %DO_MODIFY% %DO_MERGE%  %DO_COPY_EXTRACT%
 )
 
 set VECTORCAST_DIR=%ORIG_VCD%
