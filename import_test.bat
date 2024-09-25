38b0b7b16aa876e493580c7924780908ebe61523
TimSVector <tim.schneider@vector.com>
2024-09-25 08:29:09 -0400

    testing

--- import_test.bat
+++ import_test.bat
@@ -1,6 +1,7 @@
 
+set ORIG_PATH=%PATH%
 
-call %WORKSPACE%\setenv.bat > nul
+call setenv.bat > nul
 
 git clean -fxd
 git checkout HEAD 2018_fast_test\2018_fast_test.vcm
@@ -14,7 +15,6 @@ xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* %WORKSPACE%\vc_scripts > nul
 set VECTORCAST_DIR=c:\vcast\2018sp5
 %VECTORCAST_DIR%\manage -p 2018_fast_test\2018_fast_test.vcm --clean
 %VECTORCAST_DIR%\vpython %WORKSPACE%\vc_scripts\vcast_exec.py 2018_fast_test\2018_fast_test.vcm --build-execute --incremental --jobs=6 --cobertura --junit --lcov --sonarqube --aggregate --metrics --fullstatus
-pause 
 
 git clean -fxd
 git checkout HEAD 2018_fast_test\2018_fast_test.vcm
@@ -22,7 +22,6 @@ xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* %WORKSPACE%\vc_scripts > nul
 %VECTORCAST_DIR%\manage -p 2018_fast_test\2018_fast_test.vcm --clean
 set VECTORCAST_DIR=c:\vcast\2019sp6
 %VECTORCAST_DIR%\vpython %WORKSPACE%\vc_scripts\vcast_exec.py 2018_fast_test\2018_fast_test.vcm --build-execute --incremental --jobs=6 --cobertura --junit --lcov --sonarqube --aggregate --metrics --fullstatus
-pause 
 
 git clean -fxd
 git checkout HEAD 2018_fast_test\2018_fast_test.vcm
@@ -30,7 +29,6 @@ xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* %WORKSPACE%\vc_scripts > nul
 %VECTORCAST_DIR%\manage -p 2018_fast_test\2018_fast_test.vcm --clean
 set VECTORCAST_DIR=c:\vcast\2020sp7
 %VECTORCAST_DIR%\vpython %WORKSPACE%\vc_scripts\vcast_exec.py 2018_fast_test\2018_fast_test.vcm --build-execute --incremental --jobs=6 --cobertura --junit --lcov --sonarqube --aggregate --metrics --fullstatus
-pause 
 
 git clean -fxd
 git checkout HEAD 2018_fast_test\2018_fast_test.vcm
@@ -38,4 +36,5 @@ xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* %WORKSPACE%\vc_scripts > nul
 %VECTORCAST_DIR%\manage -p 2018_fast_test\2018_fast_test.vcm --clean
 set VECTORCAST_DIR=c:\vcast\2024sp3
 %VECTORCAST_DIR%\vpython %WORKSPACE%\vc_scripts\vcast_exec.py 2018_fast_test\2018_fast_test.vcm --build-execute --incremental --jobs=6 --cobertura --junit --lcov --sonarqube --aggregate --metrics --fullstatus
-pause 
+
+set PATH=%ORIG_PATH%
