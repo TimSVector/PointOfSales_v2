58ac86173213b66958dce6737f44f20f43e2ef67
TimSVector <tim.schneider@vector.com>
2024-10-18 15:07:19 -0400

    making build non-verbose

--- CurrentRelease/make_post.bat
+++ CurrentRelease/make_post.bat
@@ -28,14 +28,14 @@ if "%1"=="AIR" (
 echo %MGR_DIR%
 
 @echo Starting Make > build.log
-gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\database.o        -c database\src\database.c       --verbose   >> build.log 2>&1
-gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\encrypt.o         -c encrypt\src\encrypt.c         --verbose   >> build.log 2>&1
-gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\matrix_multiply.o -c encrypt\src\matrix_multiply.c --verbose   >> build.log 2>&1
-gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\manager.o         -c %MGR_DIR%\src\manager.c       --verbose   >> build.log 2>&1
-gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\manager_driver.o  -c main\%DRIVER_C%               --verbose   >> build.log 2>&1
-gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\waiting_list.o    -c %MGR_DIR%\src\waiting_list.c  --verbose   >> build.log 2>&1
-gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\whitebox.o        -c utils\src\whitebox.c          --verbose   >> build.log 2>&1
-gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\linked_list.o     -c utils\src\linked_list.c       --verbose   >> build.log 2>&1
+gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\database.o        -c database\src\database.c        >> build.log 2>&1
+gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\encrypt.o         -c encrypt\src\encrypt.c          >> build.log 2>&1
+gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\matrix_multiply.o -c encrypt\src\matrix_multiply.c  >> build.log 2>&1
+gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\manager.o         -c %MGR_DIR%\src\manager.c        >> build.log 2>&1
+gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\manager_driver.o  -c main\%DRIVER_C%                >> build.log 2>&1
+gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\waiting_list.o    -c %MGR_DIR%\src\waiting_list.c   >> build.log 2>&1
+gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\whitebox.o        -c utils\src\whitebox.c           >> build.log 2>&1
+gcc %COUPLING% -g -I encrypt\inc -I %MGR_DIR%\inc -I database\inc -I utils\inc -o build\linked_list.o     -c utils\src\linked_list.c        >> build.log 2>&1
 
 gcc build/*.o -o %EXEC% >> build.log 2>&1
 @echo Completed Make >> build.log
