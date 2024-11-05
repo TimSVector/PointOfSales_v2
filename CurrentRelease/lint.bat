9dc60a560f5241f7c868603b0b365a476cacdb11
TimSVector <tim.schneider@vector.com>
2024-11-05 16:17:42 -0500

    Updates

--- CurrentRelease/lint.bat
+++ CurrentRelease/lint.bat
@@ -1,17 +1,12 @@
-@echo on
+@echo off
 
 pushd %~dp0
 set FORMAT=
 set EXT=xml
 
+%VECTORCAST_DIR%/vpython -c "from datetime import datetime; print(datetime.now())"
+
 pclp64 post.lnt env-xml.lnt database\src\database.c encrypt\src\encrypt.c  encrypt\src\matrix_multiply.c  order_entry\src\manager.c main\pos_driver.c order_entry\src\waiting_list.c utils\src\whitebox.c utils\src\linked_list.c >  post_lint_results.xml
 
-REM pclp64 post.lnt %FORMAT% encrypt\src\encrypt.c          >> post_lint_results.%EXT%
-REM pclp64 post.lnt %FORMAT% encrypt\src\matrix_multiply.c  >> post_lint_results.%EXT%
-REM pclp64 post.lnt %FORMAT% order_entry\src\manager.c      >> post_lint_results.%EXT%
-REM pclp64 post.lnt %FORMAT% main\pos_driver.c              >> post_lint_results.%EXT%
-REM pclp64 post.lnt %FORMAT% order_entry\src\waiting_list.c >> post_lint_results.%EXT%
-REM pclp64 post.lnt %FORMAT% utils\src\whitebox.c           >> post_lint_results.%EXT%
-REM pclp64 post.lnt %FORMAT% utils\src\linked_list.c        >> post_lint_results.%EXT%
-
+%VECTORCAST_DIR%/vpython -c "from datetime import datetime; print(datetime.now())"
 popd
