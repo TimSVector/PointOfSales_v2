8c64878bc29db970b26da230098e7997a100808d
TimSVector <tim.schneider@vector.com>
2025-06-03 11:31:45 -0400

    update for pclint plus path

--- setenv.bat
+++ setenv.bat
@@ -14,13 +14,12 @@ rem set path=%path%;d:\vector\tools\gnat\2021\bin
 rem Set path to pclp64 to default path
 rem set PATH=%PATH%;d:\Vector\tools\pc-lint-plus\1.4.1\windows;C:\VCAST\Vector.TestInsights.Cli.1.0.125+109
 
-set path=%VECTORCAST_DIR%\mingw\bin;%path%;D:\vector\tools\pc-lint-plus\2.2\pclp
-
+set PCLP_INSTALL_DIR=D:\vector\tools\pc-lint-plus\pclp.windows.2025-beta2\pclp
 set SQ_INSTALL_DIR=D:\vector\tools\sonarqube-10.2.1.78527\bin\windows-x86-64
 
-set path=%SQ_INSTALL_DIR%\jdk-17.0.8.1\bin;%SQ_INSTALL_DIR%\build-wrapper-win-x86;%SQ_INSTALL_DIR%\sonar-scanner-5.0.1.3006-windows\bin;%PATH%
+set path=%SQ_INSTALL_DIR%\jdk-17.0.8.1\bin;%SQ_INSTALL_DIR%\build-wrapper-win-x86;%SQ_INSTALL_DIR%\sonar-scanner-5.0.1.3006-windows\bin;%VECTORCAST_DIR%\mingw\bin;%PCLP_INSTALL_DIR%;%PATH%
 
-rem set VCAST_NUM_JOBS=8
+set VCAST_NUM_JOBS=8
 
 REM goto :END
 REM if EXECUTOR_NUMBER exists and hasn't already been appended to VECTORCAST_DIR...
@@ -31,5 +30,6 @@ REM if "%VECTORCAST_DIR:~-2,1%" == "@" goto :end
 REM set VECTORCAST_DIR=%VECTORCAST_DIR%@%EXECUTOR_NUMBER%
 
 set vector_license_file=7650@vadcpctlic1.vi.vector.int
+
 
 :END
