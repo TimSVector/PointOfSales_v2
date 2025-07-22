6b2a863e728988ba4b805478f17d4e351ac18baf
TimSVector <tim.schneider@vector.com>
2025-07-22 16:28:01 -0400

    added more tests

--- CurrentRelease/coded_test/tutorial_coded_test.cpp
+++ CurrentRelease/coded_test/tutorial_coded_test.cpp
@@ -29,9 +29,43 @@ VTEST(managerTests, FailingTest) {
   VASSERT_EQ(2, 1);
 }
 
+
 VTEST_F(managerTests, ExampleFixtureTestCase, managerFixture) {
+
+  // enable stub for Add_Included_Dessert
+  // Insert data on stub
   OrderType Order;
   Order.Entree = Steak;
   mgr->PlaceOrder(1, 1, Order);
+
+  // Check stub expected values
   VASSERT_EQ(mgr->GetCheckTotal(1), 14);
+}
+
+VTEST_F(managerTests, SteakTest, managerFixture) {
+  OrderType Order;
+  Order.Entree = Steak;
+  mgr->PlaceOrder(1, 1, Order);
+  VASSERT_EQ(mgr->GetCheckTotal(1), COST_OF_STEAK);
+}
+
+VTEST_F(managerTests, LobsterTest, managerFixture) {
+  OrderType Order;
+  Order.Entree = Lobster;
+  mgr->PlaceOrder(1, 1, Order);
+  VASSERT_EQ(mgr->GetCheckTotal(1), COST_OF_LOBSTER);
+}
+
+VTEST_F(managerTests, ChickenTest, managerFixture) {
+  OrderType Order;
+  Order.Entree = Chicken;
+  mgr->PlaceOrder(1, 1, Order);
+  VASSERT_EQ(mgr->GetCheckTotal(1), COST_OF_CHICKEN);
+}
+
+VTEST_F(managerTests, PastaTest, managerFixture) {
+  OrderType Order;
+  Order.Entree = Pasta;
+  mgr->PlaceOrder(1, 1, Order);
+  VASSERT_EQ(mgr->GetCheckTotal(1), COST_OF_PASTA);
 }
