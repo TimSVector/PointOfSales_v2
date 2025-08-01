2e22fe750a2182177b79637788bbc839c019fbb8
TimSVector <tim.schneider@vector.com>
2025-08-01 17:40:02 -0400

    Update for jenkins test

--- squore_push.bat
+++ squore_push.bat
@@ -1,3 +1,5 @@
+@echo off
+
 
 set SQUORE_HOME=C:/Vector/tools/squore/24.0.4
 set SQUORE_PROJECT_NAME=PointOfSales_v2
@@ -13,7 +15,7 @@ echo %1
 :: Getting the Squore "version"
 for /f %%i in ('git rev-list --count HEAD') do set REV_COUNT=%%i
 
-if not "%JENKINS_URL%"=="" (
+if defined JENKINS_URL (
     set SQUORE_VERSION=%BUILD_TAG%-%REV_COUNT%
     set SQUORE_VERSION=%SQUORE_VERSION:~0,64%
 ) else (
@@ -39,4 +41,4 @@ if not defined WORKSPACE (
 
 echo java -jar %SQUORE_JAR% %SQUORE_URL% -t %SQUORE_TOKEN_JENKINS% -- --commands=%SQUORE_COMMANDS% --name=%SQUORE_PROJECT_NAME% --color="rgb(64,192,0)" --version="%SQUORE_VERSION%" --wizardId="ANALYTICS" --tag="CRITICAL_FACTOR=CRITICAL_FACTOR_D" --dp="type=SQuORE,languages=c:.c,.C;cpp:.cpp,.CPP,.h,.H,.cxx,.CXX,.hxx,.HXX,.cc,.CC,.hh,.HH,pattern_dir=%VCAST_DEMO_SRC_BASE%\vcast-workarea;" --dp="type=VectorCAST_API,project_file_list=%VCAST_DEMO_SRC_BASE%\vcast-workarea\vc_manage\PointOfSales_Manage.vcm,advanced_options=true,vectorcast_dir=%VECTORCAST_DIR%,vectorcast_version=Version 25" --dp="type=pclint_plus,results=%VCAST_DEMO_SRC_BASE%\pclp_post_results.xml,update_rules=true,config_files=%VCAST_DEMO_SRC_BASE%\co-gcc.lnt;%VCAST_DEMO_SRC_BASE%\env-xml.lnt;%VCAST_DEMO_SRC_BASE%\au-misra-cpp.lnt" --repository="path=%VCAST_CHECKOUT_ROOT%"
 
-java -jar %SQUORE_JAR% %SQUORE_URL% -t %SQUORE_TOKEN_JENKINS% -- --commands=%SQUORE_COMMANDS% --name=%SQUORE_PROJECT_NAME% --color="rgb(64,192,0)" --version="%SQUORE_VERSION%" --wizardId="ANALYTICS" --tag="CRITICAL_FACTOR=CRITICAL_FACTOR_D" --dp="type=SQuORE,languages=c:.c,.C;cpp:.cpp,.CPP,.h,.H,.cxx,.CXX,.hxx,.HXX,.cc,.CC,.hh,.HH,pattern_dir=%VCAST_DEMO_SRC_BASE%\vcast-workarea;" --dp="type=VectorCAST_API,project_file_list=%VCAST_DEMO_SRC_BASE%\vcast-workarea\vc_manage\PointOfSales_Manage.vcm,advanced_options=true,vectorcast_dir=%VECTORCAST_DIR%,vectorcast_version=Version 25" --dp="type=pclint_plus,results=%VCAST_DEMO_SRC_BASE%\pclp_post_results.xml,update_rules=true,config_files=%VCAST_DEMO_SRC_BASE%\co-gcc.lnt;%VCAST_DEMO_SRC_BASE%\env-xml.lnt;%VCAST_DEMO_SRC_BASE%\au-misra-cpp.lnt" --repository="path=%VCAST_CHECKOUT_ROOT%"
+:: java -jar %SQUORE_JAR% %SQUORE_URL% -t %SQUORE_TOKEN_JENKINS% -- --commands=%SQUORE_COMMANDS% --name=%SQUORE_PROJECT_NAME% --color="rgb(64,192,0)" --version="%SQUORE_VERSION%" --wizardId="ANALYTICS" --tag="CRITICAL_FACTOR=CRITICAL_FACTOR_D" --dp="type=SQuORE,languages=c:.c,.C;cpp:.cpp,.CPP,.h,.H,.cxx,.CXX,.hxx,.HXX,.cc,.CC,.hh,.HH,pattern_dir=%VCAST_DEMO_SRC_BASE%\vcast-workarea;" --dp="type=VectorCAST_API,project_file_list=%VCAST_DEMO_SRC_BASE%\vcast-workarea\vc_manage\PointOfSales_Manage.vcm,advanced_options=true,vectorcast_dir=%VECTORCAST_DIR%,vectorcast_version=Version 25" --dp="type=pclint_plus,results=%VCAST_DEMO_SRC_BASE%\pclp_post_results.xml,update_rules=true,config_files=%VCAST_DEMO_SRC_BASE%\co-gcc.lnt;%VCAST_DEMO_SRC_BASE%\env-xml.lnt;%VCAST_DEMO_SRC_BASE%\au-misra-cpp.lnt" --repository="path=%VCAST_CHECKOUT_ROOT%"
