21c635f89793c6e1106b11a6ca511b6f0195bd67
TimSVector <tim.schneider@vector.com>
2025-10-08 17:03:26 -0400

    updates

--- 2018_fast_test/run_my_test.bat
+++ 2018_fast_test/run_my_test.bat
@@ -7,9 +7,9 @@ manage -p 2018_fast_test --config=COVERAGE_TYPE=STATEMENT+BRANCH
 manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE 
 vpython d:\vector\github\PointOfSales_v2\vc_scripts\getjobs.py 2018_fast_test.vcm --type
 
-manage -p 2018_fast_test--export-result temp_result.vcr
-manage -p 2018_fast_test--clean  
-manage -p 2018_fast_test--import-result temp_result.vcr
+manage -p 2018_fast_test --export-result temp_result.vcr
+manage -p 2018_fast_test --clean  
+manage -p 2018_fast_test --import-result temp_result.vcr
 
 copy /y tutorial\c\manager_update.c tutorial\c\manager.c  
 vpython d:\vector\github\PointOfSales_v2\vc_scripts\vcast_exec.py 2018_fast_test 2018_fast_test.vcm --build-execute --jobs 6 --incremental
