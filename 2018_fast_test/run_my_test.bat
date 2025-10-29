1d0b01aea4ac04a85991dbc1784a4ded7b10ed44
TimSVector <tim.schneider@vector.com>
2025-10-29 11:12:00 -0400

    updates

--- 2018_fast_test/run_my_test.bat
+++ 2018_fast_test/run_my_test.bat
@@ -17,20 +17,20 @@ for %%V in (%VC_LIST%) do (
     git clean -fxd
     git checkout HEAD 2018_fast_test.vcm tutorial\c\manager.c
 
-    ::xcopy /E /S /Y /I D:\vector\github\vc_scripts_submodule\*.* d:\vector\github\PointOfSales_v2\vc_scripts > nul 2>&1
-    xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* d:\vector\github\PointOfSales_v2\vc_scripts > nul 2>&1
+    ::xcopy /E /S /Y /I D:\vector\github\vc_scripts_submodule\*.* %~dp0..\vc_scripts\vc_scripts > nul 2>&1
+    xcopy /E /S /Y /I %VCAST_VC_SCRIPTS%\*.* %~dp0..\vc_scripts\vc_scripts > nul 2>&1
 
     manage -p 2018_fast_test --status 
     manage -p 2018_fast_test --config=COVERAGE_TYPE=STATEMENT+BRANCH
     manage -p 2018_fast_test --config VCAST_COVERAGE_SOURCE_FILE_PERSPECTIVE=TRUE 
 
-    vpython d:\vector\github\PointOfSales_v2\vc_scripts\getjobs.py 2018_fast_test.vcm --type
+    vpython %~dp0..\vc_scripts\vc_scripts\getjobs.py 2018_fast_test.vcm --type
     if errorlevel 1 goto END
 
-    vpython d:\vector\github\PointOfSales_v2\vc_scripts\vcast_exec.py 2018_fast_test.vcm --build-execute --jobs=6 --verbose
+    vpython %~dp0..\vc_scripts\vc_scripts\vcast_exec.py 2018_fast_test.vcm --build-execute --jobs=6 --verbose
     if errorlevel 1 goto END
 
-    copy 2018_fast_test_build.log D:\vector\github\PointOfSales_v2\unstashed_build.log
+    copy 2018_fast_test_build.log %~dp0..\vc_scripts\unstashed_build.log
 
     manage -p 2018_fast_test --export-result temp_result.vcr
     manage -p 2018_fast_test --clean  
@@ -39,25 +39,25 @@ for %%V in (%VC_LIST%) do (
 
     copy /y tutorial\c\manager_update.c tutorial\c\manager.c  
 
-    vpython d:\vector\github\PointOfSales_v2\vc_scripts\vcast_exec.py 2018_fast_test.vcm --build-execute  --incremental --jobs=6 --verbose
+    vpython %~dp0..\vc_scripts\vc_scripts\vcast_exec.py 2018_fast_test.vcm --build-execute  --incremental --jobs=6 --verbose
     if errorlevel 1 goto END
 
-    copy 2018_fast_test_build.log D:\vector\github\PointOfSales_v2\unstashed_build.log
+    copy 2018_fast_test_build.log %~dp0..\vc_scripts\unstashed_build.log
 
-    vpython d:\vector\github\PointOfSales_v2\vc_scripts\vcast_exec.py 2018_fast_test.vcm --junit --cobertura_extended --lcov --aggregate --metrics --fullstatus
+    vpython %~dp0..\vc_scripts\vc_scripts\vcast_exec.py 2018_fast_test.vcm --junit --cobertura_extended --lcov --aggregate --metrics --fullstatus
     
     if errorlevel 1 goto END
 
-    IF EXIST "d:\vector\github\PointOfSales_v2\vc_scripts\generate-results.py" (
-        vpython d:\vector\github\PointOfSales_v2\vc_scripts\generate-results.py 2018_fast_test.vcm --wait_time 30 --wait_loops 1 --buildlog D:\vector\github\PointOfSales_v2\unstashed_build.log --print_exc   
+    IF EXIST "%~dp0..\vc_scripts\vc_scripts\generate-results.py" (
+        vpython %~dp0..\vc_scripts\vc_scripts\generate-results.py 2018_fast_test.vcm --wait_time 30 --wait_loops 1 --buildlog %~dp0..\vc_scripts\unstashed_build.log --print_exc   
     ) ELSE (
-        vpython d:\vector\github\PointOfSales_v2\vc_scripts\generate_results.py 2018_fast_test.vcm 
+        vpython %~dp0..\vc_scripts\vc_scripts\generate_results.py 2018_fast_test.vcm 
     )
 
     if errorlevel 1 goto END
     
-    IF EXIST "d:\vector\github\PointOfSales_v2\vc_scripts\parallel_full_reports.py" (
-        vpython d:\vector\github\PointOfSales_v2\vc_scripts\parallel_full_reports.py 2018_fast_test.vcm --jobs max
+    IF EXIST "%~dp0..\vc_scripts\vc_scripts\parallel_full_reports.py" (
+        vpython %~dp0..\vc_scripts\vc_scripts\parallel_full_reports.py 2018_fast_test.vcm --jobs max
     )
 )
 
